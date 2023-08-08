315827352254e82cb8223af41e1ab169a7c05693
Greh <81454417+grehtech@users.noreply.github.com>
2023-08-08 06:27:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,65 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine()
+
 app.get('/api/email', (req, res, next) => {
-    res.send('success')
+    res.render('success.html');
 })
 
 
 
-app.post('/api/email', async(req, res, next) => {
-    
-    let data = req.body;
+app.post('/api/email', (req, res, next) => {
 
-    let transporter = nodemailer.createTransport({
-        host: "mail.destanclassics.com.ng",
-        port: 465,
-        secure: true,
-        auth: {
-          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-          user: 'test@destanclassics.com.ng',
-          pass: 'DanielEsther@100%'
-        }
+    let data = req.body;
+    let username = process.env.SMTP_USERNAME;
+    let password = process.env.SMTP_PASSWORD;
+
+    const transporter = nodemailer.createTransport({
+      host: "ptsclinicilorin.com",
+      port: 465,
+      secure: true,
+      auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: username,
+        pass: password
+      }
+    });
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+      // send mail with defined transport object
+      const info = await transporter.sendMail({
+        from: req.body.from, // sender address
+        to: `${req.body.to}`, // list of receivers
+        subject: req.body.subject, // Subject line
+        text: req.body.message // plain text body
+        // html: //use this to send html mail 
       });
 
-    let info = transporter.sendMail({
-        from: req.body.from,
-        to: req.body.to,
-        subject: req.body.subject,
-        text: req.body.message
-    })
+      console.log("Message sent: %s", info.messageId);
+      res.send('Message sent!!!...');
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    if(info){
-        console.log('mail sent successfully')
-    }else(
-        console.log('not sents')
-    )
+      //
+      // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+      //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+      //       <https://github.com/forwardemail/preview-email>
+      //
+    }
 
+
+  main().catch(console.error);
 
 
 
